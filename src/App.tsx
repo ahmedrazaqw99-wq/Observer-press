a855a42032bcd075ca9ccec1bd50c8541b3744fd
ahmedrazaqw99-wq <ahmedrazaqw99@gmail.com>
2026-07-30 01:46:33 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Masthead } from './components/Masthead';
 import { FrontPageGrid } from './components/FrontPageGrid';
 import { ArticleReader } from './components/ArticleReader';
